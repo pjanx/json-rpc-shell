4795ee851da3efc7dd4aef0ab24aa16e89c639d4
Přemysl Eric Janouch <p@janouch.name>
2021-10-30 01:56:18 +0200

    FindLibEV.cmake: synchronise

--- cmake/FindLibEV.cmake
+++ cmake/FindLibEV.cmake
@@ -5,14 +5,16 @@
 
 # Some distributions do add it, though
 find_package (PkgConfig REQUIRED)
-pkg_check_modules (LIBEV QUIET libev)
+pkg_check_modules (LibEV QUIET libev)
 
-if (NOT LIBEV_FOUND)
-	find_path (LIBEV_INCLUDE_DIRS ev.h)
-	find_library (LIBEV_LIBRARIES NAMES ev)
+set (required_vars LibEV_LIBRARIES)
+if (NOT LibEV_FOUND)
+	find_path (LibEV_INCLUDE_DIRS ev.h)
+	find_library (LibEV_LIBRARIES NAMES ev)
+	list (APPEND required_vars LibEV_INCLUDE_DIRS)
+endif ()
 
-	if (LIBEV_INCLUDE_DIRS AND LIBEV_LIBRARIES)
-		set (LIBEV_FOUND TRUE)
-	endif (LIBEV_INCLUDE_DIRS AND LIBEV_LIBRARIES)
-endif (NOT LIBEV_FOUND)
+include (FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS (LibEV DEFAULT_MSG ${required_vars})
 
+mark_as_advanced (LibEV_LIBRARIES LibEV_INCLUDE_DIRS)
